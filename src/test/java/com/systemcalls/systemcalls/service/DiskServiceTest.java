729e8f4d45c15ee37165bd249aca0b290c662bb7
rikhila07 <rikhilaannem@gmail.com>
2024-11-20 16:14:39 -0800

    Unit tests for Disk Service

--- src/test/java/com/systemcalls/systemcalls/service/DiskServiceTest.java
+++ src/test/java/com/systemcalls/systemcalls/service/DiskServiceTest.java
@@ -1,49 +1,66 @@
-//package com.systemcalls.systemcalls.service;
-//
-//import com.systemcalls.systemcalls.domain.constants.Constants;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//
-//
-//import java.io.File;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.when;
-//
-//class DiskServiceTest {
-//
-//    @Mock
-//    private File file;
-//    @InjectMocks
-//    private DiskService diskService;
-//
-//
-//    @Test
-//    void getDiskSpaceUsedInMb_ReturnsSuccessResponse() {
-//        long testTotalSpace = 1000;
-//        long testFreeSpace = 500;
-//        when(new File("/")).thenReturn(file);
-//        when(file.getTotalSpace()).thenReturn(testTotalSpace);
-//        when(file.getFreeSpace()).thenReturn(testFreeSpace);
-//        long actualUsedSpaceInMb = diskService.getDiskSpaceUsedInMb();
-//        assertEquals((testTotalSpace-testFreeSpace)/ Constants.MB_CONVERSION_FACTOR, actualUsedSpaceInMb);
-//    }
-//
-//    @Test
-//    void getDiskSpaceUsedInMb_ReturnsException(){
-//        when(file.getTotalSpace()).thenThrow(new RuntimeException());
-//        RuntimeException exception = assertThrows(RuntimeException.class,()-> diskService.getDiskSpaceUsedInMb());
-//        assertTrue(exception.toString().contains(Constants.DISK_SPACE_USAGE_ERROR));
-//    }
-//
-//    @Test
-//    void getDiskSpaceUsedInMb() {
-//    }
-//
-//    @Test
-//    void getDiskSpacePercentageUsed() {
-//    }
-//}
+package com.systemcalls.systemcalls.service;
+
+import com.systemcalls.systemcalls.domain.constants.Constants;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class DiskServiceTest {
+
+    @Mock
+    private FileSystem fileSystem;
+
+    @InjectMocks
+    private DiskService diskService;
+
+
+    @Test
+    void testGetDiskSpaceUsedInMb_ReturnsSuccessResponse() {
+        long testTotalSpace = 1000L * Constants.MB_CONVERSION_FACTOR;
+        long testFreeSpace = 400L * Constants.MB_CONVERSION_FACTOR;
+        when(fileSystem.getTotalSpace()).thenReturn(testTotalSpace);
+        when(fileSystem.getFreeSpace()).thenReturn(testFreeSpace);
+        long usedSpaceInMb = diskService.getDiskSpaceUsedInMb();
+        assertEquals(600L, usedSpaceInMb);
+    }
+
+    @Test
+    void testGetDiskSpaceUsedInMb_ReturnsException() {
+        when(fileSystem.getTotalSpace()).thenThrow(new RuntimeException());
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            diskService.getDiskSpaceUsedInMb();
+        });
+        assertTrue(exception.getMessage().contains(Constants.DISK_SPACE_USAGE_ERROR));
+    }
+
+    @Test
+    void testGetDiskSpacePercentageUsed_ReturnsSuccessResponse() {
+        double testTotalSpace = 1000L * Constants.MB_CONVERSION_FACTOR;
+        double testFreeSpace = 400L * Constants.MB_CONVERSION_FACTOR;
+        when(fileSystem.getTotalSpace()).thenReturn((long) testTotalSpace);
+        when(fileSystem.getFreeSpace()).thenReturn((long) testFreeSpace);
+        BigDecimal percentageUsed = diskService.getDiskSpacePercentageUsed();
+        assertEquals(BigDecimal.valueOf(60.00).setScale(2), percentageUsed);
+    }
+
+    @Test
+    void testGetDiskSpacePercentageUsed_ReturnsException() {
+        when(fileSystem.getTotalSpace()).thenThrow(new RuntimeException());
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            diskService.getDiskSpacePercentageUsed();
+        });
+        assertTrue(exception.getMessage().contains(Constants.DISK_SPACE_PERCENTAGE_USAGE_ERROR));
+    }
+}
